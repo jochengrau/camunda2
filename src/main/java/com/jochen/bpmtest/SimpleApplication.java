a8730b342bf28a107223639f0da711d4b854782e
jochengrau <Jochen.Grau@web.de>
2016-05-18 14:16:04 +0200

    Initial commit

---
+++ src/main/java/com/jochen/bpmtest/SimpleApplication.java
@@ -0,0 +1,98 @@
+package com.jochen.bpmtest;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.camunda.bpm.engine.HistoryService;
+import org.camunda.bpm.engine.history.HistoricProcessInstance;
+import org.camunda.bpm.spring.boot.starter.CamundaBpmProperties;
+import org.camunda.bpm.spring.boot.starter.SpringBootProcessApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+@EnableScheduling
+public class SimpleApplication implements CommandLineRunner {
+
+  boolean contextClosed;
+
+  public static void main(final String... args) throws Exception {
+    SpringApplication.run(SimpleApplication.class, args);
+  }
+
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @Autowired
+  private HistoryService historyService;
+
+  @Autowired
+  private ConfigurableApplicationContext context;
+
+  @Autowired
+  private CamundaBpmProperties camundaBpmProperties;
+
+  @Autowired
+  private Showcase showcase;
+
+  @Autowired
+  private SpringBootProcessApplication springBootProcessApplication;
+
+  @Bean
+  public SpringBootProcessApplication processApplication() {
+    return new SpringBootProcessApplication();
+  }
+
+  @EventListener
+  public void contextClosed(ContextClosedEvent event) {
+    logger.info("context closed!");
+    contextClosed = true;
+  }
+
+  @Scheduled(fixedDelay = 1500L)
+  public void exitApplicationWhenProcessIsFinished() {
+    String processInstanceId = showcase.getProcessInstanceId();
+
+    if (processInstanceId == null) {
+      logger.info("processInstance not yet started!");
+      return;
+    }
+
+    if (isProcessInstanceFinished()) {
+      logger.info("processinstance ended!");
+
+      SpringApplication.exit(context, new ExitCodeGenerator() {
+
+        @Override
+        public int getExitCode() {
+          return 0;
+        }
+      });
+      return;
+    }
+    logger.info("processInstance not yet ended!");
+  }
+
+  public boolean isProcessInstanceFinished() {
+      final HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery()
+        .processInstanceId(showcase.getProcessInstanceId())
+        .singleResult();
+
+      return historicProcessInstance != null && historicProcessInstance.getEndTime() != null;
+
+  }
+
+  @Override
+  public void run(String... strings) throws Exception {
+    logger.error(ToStringBuilder.reflectionToString(camundaBpmProperties.getApplication(), ToStringStyle.MULTI_LINE_STYLE));
+  }
+}
---
+++ src/test/java/com/jochen/bpmtest/CamundaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jochen.bpmtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SimpleApplication.class)
+public class CamundaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
